19816f4a97413dfc1adfe868619225cc0c3901b1
Alex Radu <alex@alexradu.co>
2026-03-01 09:47:05 -0500

    feat: Add `Fields` component to capture meal planning inputs, replacing `FoodInfo` and integrating `lucide-react`.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import Header from "./components/header";
 import Map from "./components/map";
 import FoodInfo from "./components/foodInfo";
+import Fields from "./components/fields";
 
 export default function Home() {
   return (
     <>
       <Header />
       <div className="grid grid-cols-[auto_1fr] min-h-screen bg-gray-400 font-sans dark:bg-gray-400 p-4">
-        {/* change colors above */}
-        <FoodInfo />
+        <Fields />
         <Map />
       </div>
     </>
