9fb4548b96786130dacd682b9d608b8e0b3e0219
EpicCarlito <jameschef2018@gmail.com>
2026-03-01 08:59:35 -0500

    basic header

---
+++ app/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="flex w-full h-lvh bg-foreground items-center justify-center">
+      <h1 className="text-xl text-black">Mappetite AI</h1>
+    </div>
+  );
+}
