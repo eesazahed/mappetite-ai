f98df79b045a2147a90e0cc02463096d184f385e
eesa zahed <eesa@dhcp-10-31-152-159.dyn.MIT.EDU>
2026-03-01 14:09:32 -0500

    resolve merge confligt

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,17 @@ import Map from "./components/map";
 import Fields from "./components/fields";
 import Footer from "./components/footer";
 
+type Coords = {
+  lat: number;
+  lng: number;
+} | null;
+
 export default function Home() {
   const [locations, setLocations] = useState<any[]>([]);
+  const [hotelCoords, setHotelCoords] = useState<{
+    lat: number;
+    lng: number;
+  } | null>(null);
 
   const scrollContainer = useRef<HTMLDivElement>(null);
 
@@ -19,9 +28,11 @@ export default function Home() {
       <Header scrollContainer={scrollContainer as RefObject<HTMLDivElement>} />
 
       <div className="grid h-screen grid-cols-[auto_1fr] bg-(--color-brand-700) p-6 font-sans">
-        <Fields onSubmitLocations={setLocations} />
-        <Map locations={locations} />
-        <Footer />
+        <Fields
+          onSubmitLocations={setLocations}
+          onSetHotelCoords={setHotelCoords}
+        />
+        <Map locations={locations} hotelCoords={hotelCoords} /> <Footer />
       </div>
     </div>
   );
