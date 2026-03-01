ba6454691ba8844ade0bdf78515936c408b7d3a9
EpicCarlito <jameschef2018@gmail.com>
2026-03-01 09:35:10 -0500

    stopped rubber banding

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="flex w-full h-lvh bg-foreground items-center justify-center">
+    <div className="flex w-full h-lvh items-center justify-center bg-(--color-brand-900)">
       <h1 className="text-xl text-black">Mappetite AI</h1>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import FoodInfo from "./components/foodInfo";
 
 export default function Home() {
   return (
-    <>
+    <div className="h-screen overflow-y-auto overscroll-none overflow-x-hidden">
       <Header />
-      <div className="grid grid-cols-[auto_1fr] min-h-screen bg-gray-400 font-sans dark:bg-gray-400 p-4">
-        {/* change colors above */}
+
+      <div className="grid grid-cols-[auto_1fr] bg-(--color-brand-700) font-sans h-screen p-6">
         <FoodInfo />
         <Map />
       </div>
-    </>
+    </div>
   );
 }
